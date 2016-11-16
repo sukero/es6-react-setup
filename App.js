ae8a5bf836c1c796237428610cfd4c87e35ff7de
helloberries <helloberriesqz@gmail.com>
2016-11-16 16:20:17 +0800

    #7

--- App.js
+++ App.js
@@ -4,28 +4,41 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { txt: '' }
+    this.state = {
+      red: 0,
+      green: 0,
+      blue: 0
+    }
     this.update = this.update.bind(this)
   }
   update(e) {
-    this.setState({txt: e.target.value})
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red).value,
+      green: ReactDOM.findDOMNode(this.refs.green).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue).value
+    })
   }
   render() {
     return (
       <div>
-        <Widget txt={this.state.txt} update={this.update} />
+        <Slider ref="red" update={this.update} />
+        {this.state.red}
+        <br />
+        <Slider ref="green" update={this.update} />
+        {this.state.green}
+        <Slider ref="blue" update={this.update} />
+        {this.state.blue}
       </div>
     )
   }
 }
 
-const Widget = (props) => {
-  return (
-    <div>
-      <h1>{props.txt}</h1>
-      <input type="text" onChange={props.update} />
-    </div>
-  )
+class Slider extends React.Component {
+  render() {
+    return (
+        <input type="range" min="0" max="255" onChange={this.props.update} />
+    )
+  }
 }
 
 ReactDOM.render(
