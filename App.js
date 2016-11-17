f05aec8c664b389619da5c877e5a623aaffb4050
helloberries <helloberriesqz@gmail.com>
2016-11-17 16:19:05 +0800

    lifecycle methods

--- App.js
+++ App.js
@@ -11,17 +11,21 @@ class App extends React.Component {
     this.setState({val:this.state.val +1})
   }
   componentWillMount() {
-    console.log('mounting');
+    this.setState({ m: 2 })
   }
   render() {
     console.log('rendering!');
-    return <button onClick={this.update}>{this.state.val}</button>
+    return (
+      <button onClick={this.update}>
+        {this.state.val * this.state.m}
+      </button>
+    )
   }
   componentDidMount() {
-    console.log('mounted');
+    this.inc = setInterval(this.update, 500)
   }
   componentWillUnmount() {
-    console.log('bye!');
+    clearInterval(this.inc)
   }
 }
 
