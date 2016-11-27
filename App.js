d5583a524d1f6629ccc5fc2d50586bb721ed67ae
helloberries <helloberriesqz@gmail.com>
2016-11-27 16:17:33 +0800

    live jsx compiler

--- App.js
+++ App.js
@@ -1,34 +1,42 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {data: [
-      {id: 1, name: "Simon Bailey"}, {id: 2, name: "Thomas Burleson"},
-      {id: 3, name: "Will Button"}, {id: 4, name: "Ben Clinkinbeard"},
-      {id: 5, name: "Kent Dodds"}, {id: 6, name: "Trevor Ewen"}
-    ]}
+    this.state = {
+      input: '/* add your jsx here */',
+      output: '',
+      err: ''
+    }
+    this.update = this.update.bind(this);
+  }
+  update(e) {
+    let code = e.target.value;
+    try {
+      this.setState({
+        output: babel.transform(code, {
+          stage: 0,
+          loose: 'all'
+        }).code,
+        err: ''
+      });
+    } catch (err) {
+      this.setState({
+        err: err.message
+      });
+    }
   }
   render() {
-    let rows = this.state.data.map( person => {
-      return <PersonRow key={person.id} data={person} />
-    })
     return (
-      <table>
-        <tbody>{rows}</tbody>
-      </table>
-    )
+      <div>
+        <header>{this.state.err}</header>
+        <div className="container">
+          <textarea onChange={this.update} defaultValue={this.state.input}></textarea>
+          <pre>{this.state.output}</pre>
+        </div>
+      </div>
+    );
   }
-}
-
-const PersonRow = (props) => {
-  return (
-    <tr>
-      <td>{props.data.id}</td>
-      <td>{props.data.name}</td>
-    </tr>
-  )
 }
 
 export default App;
